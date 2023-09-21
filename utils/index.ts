cf42defd2086ab058860068e5044fcf1974d9051
Klippie <info@johanvanderklift.nl>
2023-09-21 21:01:07 +0200

    Added car cards

---
+++ utils/index.ts
@@ -0,0 +1,29 @@
+export async function fetchCars() {
+  const headers = {
+    'X-RapidAPI-Key': '49ad9a223dmsh58a53396b6f580ep17d185jsn77e40f358ec4',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+  }
+
+  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+    headers: headers,
+  });
+
+  const result = await response.json();
+
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
